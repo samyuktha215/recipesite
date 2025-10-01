7c2721bc217f80699dd3df0b0e6a0c96db28393d
samyuktha215 <samyuktha.basam@gmail.com>
2025-10-01 15:33:49 +0200

    updated files

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"],
+     plugins: { js }, extends: ["js/recommended"],
+      languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
 ]);
