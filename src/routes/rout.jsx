77426a20c0db939f6b555c3c3ef061516941c745
samyuktha215 <samyuktha.basam@gmail.com>
2025-10-03 16:16:00 +0200

    added contactfeature

--- src/routes/rout.jsx
+++ src/routes/rout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import{Routes,Route} from 'react-router-dom'
 import Home from '../components/home.jsx'
+import Contact from '../components/contact.jsx'
 
 
 const Rout = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}/> 
-            
+        <Route path='/contact' element={<Contact/>}/>     
     </Routes>
   )
 }
