51abd5e89fb1ed14cf330061705af61347fa19d2
evanb <evan.bergqivst@iths.se>
2025-10-13 13:14:56 +0200

    fixed import error on recipe details

--- client/src/components/recipes/RecipeDetails.jsx
+++ client/src/components/recipes/RecipeDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./RecipeDetailsPage.css";
 import Sidebar from "../../pages/sidebar";
  
 export default function RecipeDetailsPage() {
